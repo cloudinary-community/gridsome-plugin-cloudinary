58ded5e7c3878731f1ad3b8d281d7efcad2c3b81
Maya Shavin <maya@cloudinary.com>
2020-05-31 22:57:55 +0300

    in it

---
+++ src/gridsome.client.js
@@ -0,0 +1,14 @@
+export default function (Vue, options) {
+  const { 
+    default: Cloudinary,
+    CldImage,
+    CldTransformation,
+    CldVideo,
+    CldContext,
+     } = require('cloudinary-vue');  
+
+  Vue.use(Cloudinary, {
+    ...options,
+    components: [ CldContext, CldImage, CldTransformation, CldVideo ]
+  })
+}
