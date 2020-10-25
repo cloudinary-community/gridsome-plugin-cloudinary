23e3bfac5d8cd72c042be8088c895e813281a496
Maya Shavin <maya@cloudinary.com>
2020-10-25 14:02:31 +0200

    bump

--- packages/demo/gridsome.config.js
+++ packages/demo/gridsome.config.js
@@ -9,7 +9,7 @@ module.exports = {
   siteName: 'Gridsome',
   plugins: [
     {
-      use: '@mayashavin/gridsome-source-cld',
+      use: '@mayas/gridsome-source-cld',
       options: {
         cloudName: process.env.CLOUDNAME,
         apiKey: process.env.API_KEY,
